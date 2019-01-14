126258ee2cf36501de427b8038d6e84b24cc7de2
zhengwei <zhenghaku@gmail.com>
2019-01-14 16:17:22 +0800

    more test

---
+++ Code/WobsocketEventTest/Dummy/EventEcho.cs
@@ -0,0 +1,26 @@
+﻿using Jtext103.CFET2.Core;
+using Jtext103.CFET2.Core.Event;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jtext103.CFET2.WebsocketEvent.Test.Dummy
+{
+    public class EventEcho:Thing
+    {
+        public override void TryInit(object initObj)
+        {
+            base.TryInit(initObj);
+            //here give it a list of event to subscribe, tpye: defualt
+        }
+
+        public void echo(EventArg e)
+        {
+            //fire each event as: /echo/{original event path} 
+            //sample as old event sample
+        }
+
+    }
+}
